1679f45dbd061bb7c113748920c2d12f70ed9e6a
bo-codes <bo.coding888@gmail.com>
2022-06-06 16:46:43 -0700

    fixed browser connection

--- routes/mangas.js
+++ routes/mangas.js
@@ -10,22 +10,22 @@ router.get("/", async(req,res) => {
   res.render('mangas', {mangas});
 })
 
-// router.get("/:id", async(req,res) => {
-//   const manga = await Manga.findByPk(req.params.id);
-//   res.render('manga', {manga});
-// })
+router.get("/:id", async(req,res) => {
+  const manga = await db.Manga.findByPk(req.params.id);
+  res.render('manga', {manga});
+})
 
-// router.get("/:id/reviews", async(req,res) => {
-//   const id = req.params.id
-//   const reviews = await Review.findAll({
-//     where: { mangaId: id },
-//     include: [
-//       { model: Manga, as: "manga" },
-//       { model: User, as: "user" },
-//     ],
-//   });
-//   res.render('reviews', {reviews});
-// })
+router.get("/:id/reviews", async(req,res) => {
+  const id = req.params.id
+  const reviews = await db.Review.findAll({
+    where: { mangaId: id },
+    include: [
+      { model: db.Manga, as: "Manga" },
+      { model: db.User, as: "user" },
+    ],
+  });
+  res.render('reviews', {reviews});
+})
 
 
 
