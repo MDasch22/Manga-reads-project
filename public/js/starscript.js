7286e1a873f0e2850ca60475087ae2f2a86147f4
bo-codes <bo.coding888@gmail.com>
2022-06-10 23:39:21 -0700

    got the stars @dan

---
+++ public/js/starscript.js
@@ -0,0 +1,17 @@
+const allStars = document.querySelectorAll('.star')
+let currentRating = document.querySelector('.current_rating');
+let numberRating = document.querySelector('#rating');
+
+allStars.forEach( (star,i)=> {
+  star.onclick = (e) => {
+    e.preventDefault();
+    let currentStar = i+1;
+    currentRating.innerHTML = `${currentStar} / 5`
+    allStars.forEach( (star,j) => {
+      if (currentStar >= j+1) star.innerHTML = '&#9733';
+      else star.innerHTML = '&#9734'
+    })
+    rating.value = currentStar
+    review.rating = currentStar;
+  }
+})
